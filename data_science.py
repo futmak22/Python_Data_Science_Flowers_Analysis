b5e4a6d9b8b8ddded9bbfe82c8e5df526a5388fe
Julio Cesar G.E <juliogalvez90@gmail.com>
2022-09-20 15:38:22 -0500

    Upload flowers .csv file

--- data_science.py
+++ data_science.py
@@ -2,3 +2,15 @@
 # Julio Cesar Galvez Espinosa
 #-----------------------------------------------------
 
+import numpy as np
+import pandas as pd
+import seaborn as sns  #Generador de graficas
+import matplotlib.pyplot as plt #Muestra de graficas
+from scipy import stats #Calculo coeficiente de correlación entre medidas
+from statsmodels.stats.weightstats import ztest #Evaluación de hipotesis
+import statsmodels.api as sm #Generador formula para Regresión Logística.
+import os
+
+# 1) - Extracción del path de la data de flores.
+irisfile_path = os.getcwd() + '/data_sets/iris_data.csv'
+
